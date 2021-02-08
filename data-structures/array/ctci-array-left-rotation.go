4939982cc1700483caa6e1fed38a51a8b9726271
A Fakhrurozi S <haifahrul@gmail.com>
2021-02-08 22:37:51 +0700

    fix algoritma circulaer array

--- data-structures/array/ctci-array-left-rotation.go
+++ data-structures/array/ctci-array-left-rotation.go
@@ -7,23 +7,29 @@ func main() {
 }
 
 func test1() {
-	a := []int32{1, 2, 3, 4, 5}
-	d := 4 // number of rotation
-	// a := []int32{41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51}
-	// d := 10 // number of rotation
-	l := len(a)
+	a := []int32{1, 2, 3, 4, 5, 6}
+	d := int32(4) // number of rotation
+	a2 := []int32{41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51}
+	d2 := int32(10) // number of rotation
 
+	fmt.Println("Input   :  1 2 3 4 5 6")
+	fmt.Println("Result  :", calculate(a, d))
+	fmt.Println("Expected:  5 6 1 2 3 4")
+	fmt.Println("")
+	fmt.Println("Input   :  41 73 89 7 10 1 59 58 84 77 77 97 58 1 86 58 26 10 86 51")
+	fmt.Println("Result  :", calculate(a2, d2))
+	fmt.Println("Expected:  77 97 58 1 86 58 26 10 86 51 41 73 89 7 10 1 59 58 84 77")
+}
+
+func calculate(arr []int32, d int32) []int32 {
+	var i, l int32
+	l = int32(len(arr))
 	tmp := make([]int32, l)
 
-	for i := 0; i < l; i++ {
-		if i < d {
-			tmp[i] = a[d+i]
-		} else {
-			tmp[i] = a[i-d]
-		}
+	for i = 0; i < l; i++ {
+		j := (i + d) % l
+		tmp[i] = arr[j]
 	}
 
-	fmt.Println("Result  :", tmp)
-	fmt.Println("Expected: 77 97 58 1 86 58 26 10 86 51 41 73 89 7 10 1 59 58 84 77")
-	fmt.Println("Expected: 5 1 2 3 4")
+	return tmp
 }
