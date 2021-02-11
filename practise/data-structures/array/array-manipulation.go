01ddf5c1771b0df37bf8d77cda0bde95a07303c1
A Fakhrurozi S <haifahrul@gmail.com>
2021-02-11 20:21:06 +0700

    change structure folder

---
+++ practise/data-structures/array/array-manipulation.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 10
+	queries := [][]int32{
+		{1, 5, 3},
+		{4, 8, 7},
+		{6, 9, 1},
+	}
+	// queries := [][]int32{
+	// 	{1, 2, 100},
+	// 	{2, 5, 100},
+	// 	{3, 4, 100},
+	// }
+
+	for i := 3; i <= n; i++ {
+		for i := 1; i <= 2*n; i++ {
+
+		}
+	}
+
+	fmt.Println(n, queries)
+}
